1d2d27e80d998ef9fa36c8aa6013dc23b943a692
Dominik Wilk <mr.wilkd@gmail.com>
2023-12-15 12:17:53 +0100

    Add files for public project

---
+++ build/precache-manifest.c85844685216c14976d8edfb3ea964c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f8fcfc488f7f54f844437d83441f4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "722f5eafdec72369d304",
+    "url": "/static/css/main.bfa03f71.chunk.css"
+  },
+  {
+    "revision": "5ab21fe70d8a6bd15205",
+    "url": "/static/js/2.e1ce96a3.chunk.js"
+  },
+  {
+    "revision": "722f5eafdec72369d304",
+    "url": "/static/js/main.4a0dcb49.chunk.js"
+  },
+  {
+    "revision": "ec396816c40c610230cc",
+    "url": "/static/js/runtime-main.a417407a.js"
+  }
+]);
